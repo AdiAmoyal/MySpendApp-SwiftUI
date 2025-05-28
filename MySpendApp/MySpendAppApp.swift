9b6118e70d990908e928c4af9a4c2259cea533a6
AdiAmoyal <adi.amoyal02@gmail.com>
2025-05-28 10:02:23 +0300

    Initial Commit

---
+++ MySpendApp/MySpendAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpendAppApp.swift
+//  MySpendApp
+//
+//  Created by Adi Amoyal on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MySpendAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySpendAppTests/MySpendAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySpendAppTests.swift
+//  MySpendAppTests
+//
+//  Created by Adi Amoyal on 28/05/2025.
+//
+
+import Testing
+@testable import MySpendApp
+
+struct MySpendAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
