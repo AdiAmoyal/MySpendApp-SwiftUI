28e2043d1af9d326b361fccf7d34c7610a3fd63e
AdiAmoyal <adi.amoyal02@gmail.com>
2025-05-28 12:50:06 +0300

    Add WelcomeView UI

--- MySpendApp/Core/Welcome/WelcomeView.swift
+++ MySpendApp/Core/Welcome/WelcomeView.swift
@@ -10,18 +10,46 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome!")
-                    .frame(maxHeight: .infinity)
-                
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get Started")
-                        .callToActionButton()
-                }
+            VStack(spacing: 24) {
+                Spacer()
+                imageSection
+                titleSection
+                Spacer()
+                Spacer()
+                getStartedButton
             }
             .padding(16)
+        }
+    }
+    
+    private var imageSection: some View {
+        Image(systemName: "creditcard.fill")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 100, height: 100)
+            .foregroundStyle(.accent)
+    }
+    
+    private var titleSection: some View {
+        VStack(spacing: 8) {
+            Text("Welcome to MySpend")
+                .font(.title)
+                .fontWeight(.heavy)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Text("Track your spending, stay on budget, and gain insights into your expenses")
+                .multilineTextAlignment(.center)
+                .font(.title3)
+                .fontWeight(.light)
+        }
+    }
+    
+    private var getStartedButton: some View {
+        NavigationLink {
+            OnboardingCompletedView()
+        } label: {
+            Text("Get Started")
+                .callToActionButton()
         }
     }
 }
